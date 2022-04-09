26903c69431d156709793cc4e0e92183131d73b3
Kate Hubkina <ekaterinahubkina@yandex.by>
2022-04-10 00:01:52 +0300

    fix port

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const { register } = require('./middlewares/validation');
 const ErrorNotFound = require('./errors/ErrorNotFound');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(express.json());
